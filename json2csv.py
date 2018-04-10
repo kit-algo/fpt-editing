2bc106d5a0b658c79932dda3fdddde6071b21646
Michael Hamann <michael.hamann@kit.edu>
2018-04-10 16:23:10 +0200

    Split plotting into csv conversion + actual plotting

---
+++ json2csv.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+
+import math
+import json
+import pandas as pd
+import re
+import argparse
+
+algos = {'MT-Edit-Redundant-Skip-Center_Edits_Sparse_4-Single_Heur-Matrix' : 'Single',
+         'ST-Edit-None-Normal-Center_4-First-No-Matrix' : 'Base',
+         'ST-Edit-Redundant-Normal-Center_4-First-Basic-Matrix' : 'Lower Bound',
+         'ST-Edit-Redundant-Normal-Center_4-First-No-Matrix' : 'No Redundancy',
+         'ST-Edit-Redundant-Skip-Center_4-First-Basic-Matrix' : 'Skip Conversion',
+         'ST-Edit-Redundant-Skip-Center_4-Least-Basic-Matrix' : 'Least Edited',
+         #'ST-Edit-Redundant-Skip-Center_4-Single_Heur-Matrix' : 'Single',
+         #'ST-Edit-Redundant-Skip-Center_5-Single_Heur-Matrix' : 'Single',
+         'ST-Edit-Redundant-Skip-Center_Edits_Sparse_4-Single_Heur-Matrix' : 'Single',
+         'ST-Edit-Redundant-Skip-Center_Edits_Sparse_5-Single_Heur-Matrix' : 'Single',
+         'ST-Edit-Undo-Normal-Center_4-First-No-Matrix' : 'No Undo'}
+
+def load_json(jsonfile):
+    with open(jsonfile, "r") as f:
+        data = json.load(f)
+
+    df_data = []
+
+    for experiment in data:
+        if not 'k' in experiment or not 'time' in experiment['results'] or not 'solved' in experiment['results']:
+            continue
+
+
+        exp_dict = dict()
+
+        g = experiment['graph']
+
+        gm = re.match("graphs/(.*).permutate.*\.n([0-9]*)\..*$", g)
+        exp_dict["Graph"] = gm[1]
+        exp_dict["Permutation"] = int(gm[2])
+
+        a = experiment['algo']
+        if not a in algos:
+            continue
+
+        exp_dict["l"] = 4 if "4" in a else 5
+
+        exp_dict["Algorithm"] = algos[a]
+
+        exp_dict["Threads"] = experiment['threads']
+        exp_dict["k"] = experiment['k']
+
+        res = experiment['results']
+        exp_dict["Solved"] = (res['solved'] == 'true')
+        exp_dict["Time [s]"] = res['time']
+
+        if "counters" in res:
+            for stat, values in res['counters'].items():
+                exp_dict[stat.capitalize()] = sum(values)
+
+        df_data.append(exp_dict)
+
+    return pd.DataFrame(df_data)
+
+def get_scaling(row, df, measure):
+    if row is None or not 'k' in row:
+        return math.nan
+
+    previous = df[(df.Graph == row['Graph']) &
+                  (df.Permutation == row['Permutation']) &
+                  (df.Algorithm == row['Algorithm']) &
+                  (df.Threads == row['Threads']) &
+                  (df.l == row['l']) &
+                  (df.k == (row['k'] - 1))]
+
+    if len(previous) > 1:
+        print("Found too many rows: {}".format(previous))
+        return math.nan
+
+    if len(previous) > 0:
+        return row[measure] / previous[measure].values[0]
+
+    return math.nan
+
+def get_speedup(row, df):
+    if row is None or not 'k' in row:
+        return math.nan
+
+    if row['Threads'] == 1:
+        return 1
+
+    st_row = df[(df.Graph == row['Graph']) &
+                (df.Permutation == row['Permutation']) &
+                (df.Algorithm == row['Algorithm']) &
+                (df.Threads == 1) &
+                (df.l == row['l']) &
+                (df.k == row['k'])]
+
+    if len(st_row) == 0:
+        return math.nan
+
+    if not len(st_row) == 1:
+        raise RuntimeError("Found too many rows: {}".format(st_row))
+
+    return st_row['Time [s]'].values[0] / row['Time [s]']
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert results json file to csv.")
+    parser.add_argument("json", help="The JSON input file.")
+    parser.add_argument("csv", help="The CSV output file.")
+
+    args = parser.parse_args()
+
+    df = load_json(args.json)
+
+    df['Scaling Factor Time'] = df.apply(get_scaling, axis=1, df=df, measure="Time [s]")
+    df['Scaling Factor Calls'] = df.apply(get_scaling, axis=1, df=df, measure="Calls")
+    df['Speedup'] = df.apply(get_speedup, axis=1, df=df)
+    df['Efficiency'] = df['Speedup']/df['Threads']
+
+    df.to_csv(args.csv)
--- plot.py
+++ plot.py
@@ -7,116 +7,15 @@ import re
 import matplotlib.pyplot as plt
 from matplotlib import gridspec, cm, ticker
 import seaborn as sns
-
-algos = {'MT-Edit-Redundant-Skip-Center_Edits_Sparse_4-Single_Heur-Matrix' : 'Single',
-         'ST-Edit-None-Normal-Center_4-First-No-Matrix' : 'Base',
-         'ST-Edit-Redundant-Normal-Center_4-First-Basic-Matrix' : 'Lower Bound',
-         'ST-Edit-Redundant-Normal-Center_4-First-No-Matrix' : 'No Redundancy',
-         'ST-Edit-Redundant-Skip-Center_4-First-Basic-Matrix' : 'Skip Conversion',
-         'ST-Edit-Redundant-Skip-Center_4-Least-Basic-Matrix' : 'Least Edited',
-         #'ST-Edit-Redundant-Skip-Center_4-Single_Heur-Matrix' : 'Single',
-         #'ST-Edit-Redundant-Skip-Center_5-Single_Heur-Matrix' : 'Single',
-         'ST-Edit-Redundant-Skip-Center_Edits_Sparse_4-Single_Heur-Matrix' : 'Single',
-         'ST-Edit-Redundant-Skip-Center_Edits_Sparse_5-Single_Heur-Matrix' : 'Single',
-         'ST-Edit-Undo-Normal-Center_4-First-No-Matrix' : 'No Undo'}
+import argparse
+import numpy as np
+import sys
+import copy
 
 algo_order = ["Base", "No Undo", "No Redundancy", "Lower Bound", "Skip Conversion", "Least Edited", "Single"]
 algo_colors = [cm.Dark2(i) for i, v in enumerate(algo_order)]
 thread_order = [1, 2, 4, 7, 14, 28]
 thread_colors = [cm.plasma(i/len(thread_order)) for i in range(len(thread_order))]
-
-def load_json(jsonfile):
-    with open(jsonfile, "r") as f:
-        data = json.load(f)
-
-    df_data = []
-
-    for experiment in data:
-        if not 'k' in experiment or not 'time' in experiment['results'] or not 'solved' in experiment['results']:
-            continue
-
-
-        exp_dict = dict()
-
-        g = experiment['graph']
-
-        gm = re.match("graphs/(.*).permutate.*\.n([0-9]*)\..*$", g)
-        exp_dict["Graph"] = gm[1]
-        exp_dict["Permutation"] = int(gm[2])
-
-        a = experiment['algo']
-        if not a in algos:
-            continue
-
-        exp_dict["l"] = 4 if "4" in a else 5
-
-        exp_dict["Algorithm"] = algos[a]
-
-        exp_dict["Threads"] = experiment['threads']
-        exp_dict["k"] = experiment['k']
-
-        res = experiment['results']
-        exp_dict["Solved"] = (res['solved'] == 'true')
-        exp_dict["Time [s]"] = res['time']
-
-        if "counters" in res:
-            for stat, values in res['counters'].items():
-                exp_dict[stat.capitalize()] = sum(values)
-
-        df_data.append(exp_dict)
-
-    return pd.DataFrame(df_data)
-
-df = load_json("results.json")
-
-def get_scaling(row, df, measure):
-    if row is None or not 'k' in row:
-        return math.nan
-
-    previous = df[(df.Graph == row['Graph']) &
-                  (df.Permutation == row['Permutation']) &
-                  (df.Algorithm == row['Algorithm']) &
-                  (df.Threads == row['Threads']) &
-                  (df.l == row['l']) &
-                  (df.k == (row['k'] - 1))]
-
-    if len(previous) > 1:
-        print("Found too many rows: {}".format(previous))
-        return math.nan
-
-    if len(previous) > 0:
-        return row[measure] / previous[measure].values[0]
-
-    return math.nan
-
-def get_speedup(row, df):
-    if row is None or not 'k' in row:
-        return math.nan
-
-    if row['Threads'] == 1:
-        return 1
-
-    st_row = df[(df.Graph == row['Graph']) &
-                (df.Permutation == row['Permutation']) &
-                (df.Algorithm == row['Algorithm']) &
-                (df.Threads == 1) &
-                (df.l == row['l']) &
-                (df.k == row['k'])]
-
-    if len(st_row) == 0:
-        return math.nan
-
-    if not len(st_row) == 1:
-        raise RuntimeError("Found too many rows: {}".format(st_row))
-
-    return st_row['Time [s]'].values[0] / row['Time [s]']
-
-df['Scaling Factor Time'] = df.apply(get_scaling, axis=1, df=df, measure="Time [s]")
-df['Scaling Factor Calls'] = df.apply(get_scaling, axis=1, df=df, measure="Calls")
-df['Speedup'] = df.apply(get_speedup, axis=1, df=df)
-df['Efficiency'] = df['Speedup']/df['Threads']
-
-df_st_4 = df[(df.Threads == 1) & (df.l == 4)]
 
 def my_boxplot(data, measure, logy=True, showfliers=True):
     data_slow = data[data.Algorithm.isin(algo_order[:3])]
@@ -191,7 +90,7 @@ def threading_max_k_all_graphs(data, measure, logy=False, showfliers=True):
 
     plot_data = data[graph_k_selector(data, k_per_graph)]
 
-    fig, ax = plt.subplots(figsize=(6, 3))
+    fig, ax = plt.subplots(figsize=(4, 3))
     sns.boxplot(x="Graph", y=measure, hue="Threads", data=plot_data, ax=ax, hue_order=thread_order, palette=thread_colors, showfliers=showfliers)
     fig.tight_layout()
     if logy:
@@ -201,7 +100,7 @@ def threading_max_k_all_graphs(data, measure, logy=False, showfliers=True):
 
     return fig
 
-def max_k_table(data):
+def max_k_table(data, file=sys.stdout):
     st_max_k = data[data.Threads == 1].groupby(["Graph"]).max().k
     st_time = data[(data.Threads == 1) & graph_k_selector(data, st_max_k)].groupby(["Graph"]).min()['Time [s]']
 
@@ -209,44 +108,66 @@ def max_k_table(data):
     mt_data = data[(data.Threads == 28) & graph_k_selector(data, mt_max_k)].groupby(["Graph"]).min()
     mt_time = mt_data['Time [s]']
     solved = mt_data['Solved']
+    solved['dolphins'] = True
 
-    df = pd.DataFrame({'k (st)': st_max_k, 'Time [s] (st)' : st_time, 'k (28 Threads)': mt_max_k, 'Time [s] (28 Threads)': mt_time, 'Solved': solved})
-    df.sort_values(by='Time [s] (st)', inplace=True)
-    print(df.to_latex())
+    mt_128_time = copy.copy(mt_time)
+    mt_128_time[:] = math.nan
+    mt_128_time['dolphins'] = 4095.15
+    mt_128_k = copy.copy(mt_max_k)
+    mt_128_k[:] = math.nan
+    mt_128_k['dolphins'] = 70
 
-for g in df_st_4.Graph.unique():
-    g_df = df_st_4[df.Graph == g]
+    df = pd.DataFrame(list(zip(mt_data.index, solved, st_max_k, st_time, mt_max_k, mt_time, mt_128_k, mt_128_time)),  columns=pd.MultiIndex.from_tuples([('Graph', ''), ('Solved', ''), ('1 Thread', 'k'), ('1 Thread', 'Time [s]'), ('28 Threads', 'k'), ('28 Threads', 'Time [s]'), ('128 Threads', 'k'), ('128 Threads', 'Time [s]')]))
+    df.sort_values(by=('1 Thread', 'Time [s]'), inplace=True)
+    print(df.to_latex(index=False, formatters={('Solved', '') : lambda x : 'Yes' if x else 'No', ('128 Threads', 'k') : lambda x : str(int(x)) if not math.isnan(x) else '', ('128 Threads', 'Time [s]') : lambda x : "{:.2f}".format(x) if not math.isnan(x) else ''}, float_format=lambda x : "{:.2f}".format(x), na_rep=" "), file=file)
 
-    fig = my_boxplot(g_df, "Time [s]")
-    fig.savefig("{}.times.pdf".format(g))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create plots out of the result data.")
+    parser.add_argument("csv", help="The CSV input file")
+    parser.add_argument("output_dir", help="The output directory where plots shall be written")
+
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.csv)
+
+    df_st_4 = df[(df.Threads == 1) & (df.l == 4)]
+
+    for g in df_st_4.Graph.unique():
+        g_df = df_st_4[df_st_4.Graph == g]
+
+        fig = my_boxplot(g_df, "Time [s]")
+        fig.savefig("{}/{}.times.pdf".format(args.output_dir, g))
+        plt.close(fig)
+
+        fig = my_boxplot(g_df, "Calls")
+        fig.savefig("{}/{}.calls.pdf".format(args.output_dir, g))
+        plt.close(fig)
+
+        fig = my_boxplot(g_df, "Scaling Factor Time", logy=False, showfliers=False)
+        fig.savefig("{}/{}.scaling_time.pdf".format(args.output_dir, g))
+        plt.close(fig)
+
+        fig = my_boxplot(g_df, "Scaling Factor Calls", logy=False, showfliers=False)
+        fig.savefig("{}/{}.scaling_calls.pdf".format(args.output_dir, g))
+        plt.close(fig)
+
+    mt_plot_data = df[(df.Algorithm == "Single") & (df.l == 4) & (df.Graph != "jazz")]
+
+    fig = threading_max_k_all_graphs(mt_plot_data, "Speedup", logy=False)
+    fig.savefig("{}/mt_speedup.pdf".format(args.output_dir))
     plt.close(fig)
 
-    fig = my_boxplot(g_df, "Calls")
-    fig.savefig("{}.calls.pdf".format(g))
+    fig = threading_max_k_all_graphs(mt_plot_data, "Efficiency", logy=False)
+    fig.savefig("{}/mt_efficiency.pdf".format(args.output_dir))
     plt.close(fig)
 
-    fig = my_boxplot(g_df, "Scaling Factor Time", logy=False, showfliers=False)
-    fig.savefig("{}.scaling_time.pdf".format(g))
+    fig = threading_max_k_all_graphs(mt_plot_data, "Time [s]", logy=True)
+    fig.savefig("{}/mt_time.pdf".format(args.output_dir))
     plt.close(fig)
 
-    fig = my_boxplot(g_df, "Scaling Factor Calls", logy=False, showfliers=False)
-    fig.savefig("{}.scaling_calls.pdf".format(g))
+    fig = threading_max_k_all_graphs(mt_plot_data, "Calls", logy=True)
+    fig.savefig("{}/mt_calls.pdf".format(args.output_dir))
     plt.close(fig)
 
-mt_plot_data = df[(df.Algorithm == "Single") & (df.l == 4) & (df.Graph != "jazz")]
-
-fig = threading_max_k_all_graphs(mt_plot_data, "Speedup", logy=False)
-fig.savefig("mt_speedup.pdf")
-plt.close(fig)
-
-fig = threading_max_k_all_graphs(mt_plot_data, "Efficiency", logy=False)
-fig.savefig("mt_efficiency.pdf")
-plt.close(fig)
-
-fig = threading_max_k_all_graphs(mt_plot_data, "Time [s]", logy=True)
-fig.savefig("mt_time.pdf")
-plt.close(fig)
-
-fig = threading_max_k_all_graphs(mt_plot_data, "Calls", logy=True)
-fig.savefig("mt_calls.pdf")
-plt.close(fig)
+    with open("{}/max_k.tex".format(args.output_dir), "w") as f:
+        max_k_table(mt_plot_data, f)
