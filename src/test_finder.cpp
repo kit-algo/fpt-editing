b806b6233342fa894f65473ce92569b9ac9eec00
Michael Hamann <michael.hamann@kit.edu>
2019-04-17 17:21:14 +0200

    test_finder: fix bugs, test forbidden support

--- src/test_finder.cpp
+++ src/test_finder.cpp
@@ -42,59 +42,98 @@ int main(int argc, char * argv[])
 		auto finder = std::get<i.value>(finders);
 		constexpr size_t length = decltype(finder)::length;
 		using subgraph_t = std::array<VertexID, length>;
-		using Conversion = typename decltype(finder)::Conversion;
 
 		std::cout << "Testing " << decltype(finder)::name << std::endl;
 
-		// Global listing
-		Value_Matrix<std::vector<subgraph_t>> subgraphs_per_pair(graph.size());
+		E forbidden(graph.size());
 
-		auto cb = [&](const subgraph_t& sg)
+		auto print_subgraphs = [](const std::vector<subgraph_t>& subgraphs)
+				       {
+						for (subgraph_t sg : subgraphs)
+						{
+							for (VertexID x : sg)
+								std::cout << static_cast<size_t>(x) << ", ";
+							std::cout << std::endl;
+						}
+				       };
+		auto verify_for_forbidden_pair = [&](VertexID uu, VertexID vv)
 		{
-			Finder::for_all_edges_unordered<M, R, Conversion>(graph, edited, sg.begin(), sg.end(), [&](auto u, auto v)
+			if (uu != vv) forbidden.set_edge(uu, vv);
+
+			// Global listing
+			Value_Matrix<std::vector<subgraph_t>> subgraphs_per_pair(graph.size());
+
+			auto cb = [&](const subgraph_t& sg)
 			{
-				subgraphs_per_pair.at(*u, *v).push_back(sg);
-				return false;
-			});
-			return false;
-		};
-
-		finder.find(graph, cb);
-
-		subgraphs_per_pair.forAllNodePairs([&](VertexID u, VertexID v, std::vector<subgraph_t>& subgraphs)
-		{
-			std::vector<subgraph_t> local_subgraphs;
-
-			auto local_cb = [&](const subgraph_t &sg)
-			{
-				local_subgraphs.push_back(sg);
-
+				Finder::for_all_edges_unordered<M, R, Options::Conversions::Normal>(graph, edited, sg.begin(), sg.end(), [&](auto u, auto v)
+				{
+					subgraphs_per_pair.at(*u, *v).push_back(sg);
+					return false;
+				});
 				return false;
 			};
 
-			finder.find_near(graph, u, v, local_cb);
+			if (uu != vv) finder.find(graph, cb, forbidden);
+			else finder.find(graph, cb);
 
-			if (subgraphs.size() != local_subgraphs.size())
+			if (uu != vv && subgraphs_per_pair.at(uu, vv).size() > 0)
 			{
-				std::cout << "(" << static_cast<size_t>(u) << ", " << static_cast<size_t>(v) << ")" << std::endl;
-
-				std::cout << "near (" << local_subgraphs.size() << "): " << std::endl;
-				for (subgraph_t sg : local_subgraphs)
+				for (subgraph_t& sg : subgraphs_per_pair.at(uu, vv))
 				{
-					for (VertexID x : sg)
-						std::cout << static_cast<size_t>(x) << ", ";
-					std::cout << std::endl;
-				}
-
-				std::cout << "global (" << subgraphs.size() << "): " << std::endl;
-				for (subgraph_t sg : subgraphs)
-				{
-					for (VertexID x : sg)
-						std::cout << static_cast<size_t>(x) << ", ";
-					std::cout << std::endl;
+					if ((sg.front() != uu || sg.back() != vv) && (sg.front() != vv || sg.back() != uu)) {
+						std::cout << "At forbidden pair (" << uu << ", " << vv << ")" << std::endl;
+						print_subgraphs(subgraphs_per_pair.at(uu, vv));
+					}
 				}
 			}
-		});
+
+			subgraphs_per_pair.forAllNodePairs([&](VertexID u, VertexID v, std::vector<subgraph_t>& subgraphs)
+			{
+				std::vector<subgraph_t> local_subgraphs;
+
+				auto local_cb = [&](const subgraph_t &sg)
+				{
+					local_subgraphs.push_back(sg);
+
+					return false;
+				};
+
+				bool is_forbidden_pair = (uu == u && vv == v) || (uu == v && vv == u);
+
+				if (uu != vv)
+				{
+					if (!is_forbidden_pair) finder.find_near(graph, u, v, local_cb, forbidden);
+				}
+				else finder.find_near(graph, u, v, local_cb);
+
+				if (!is_forbidden_pair && subgraphs.size() != local_subgraphs.size())
+				{
+					std::cout << "(" << static_cast<size_t>(u) << ", " << static_cast<size_t>(v) << ")" << std::endl;
+
+					std::cout << "near (" << local_subgraphs.size() << "): " << std::endl;
+
+					print_subgraphs(local_subgraphs);
+
+					std::cout << "global (" << subgraphs.size() << "): " << std::endl;
+
+					print_subgraphs(subgraphs);
+				}
+			});
+
+
+			if (uu != vv) forbidden.clear_edge(uu, vv);
+		};
+
+		verify_for_forbidden_pair(0, 0);
+
+		for (VertexID u = 0; u < graph.size(); ++u)
+		{
+			for (VertexID v = u + 1; v < graph.size(); ++v)
+			{
+				verify_for_forbidden_pair(u, v);
+			}
+		}
+
 	});
 
 	return 0;
