2d1a38d94f40f4e3934a5dfc69d1bffb8a3e861e
Michael Hamann <michael.hamann@kit.edu>
2019-07-25 18:29:44 +0200

    Support writing graph in QTM script

--- run_qtm.py
+++ run_qtm.py
@@ -5,6 +5,7 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description="Run QTM for all supplied graphs")
+parser.add_argument('output_dir')
 parser.add_argument('graphs', nargs='+')
 
 args = parser.parse_args()
@@ -17,13 +18,18 @@ for graph_path in args.graphs:
     G.indexEdges()
 
     min_k = G.numberOfEdges()
+    best_result = None
     for i in range(16):
         lin = community.QuasiThresholdEditingLinear(G).run()
         qtm = community.QuasiThresholdEditingLocalMover(G, lin.getParents(), G.numberOfEdges() * 10)
         qtm.run()
         if qtm.getNumberOfEdits() < min_k:
             min_k = qtm.getNumberOfEdits()
+            best_result = qtm.getQuasiThresholdGraph()
 
-    graph_name, _ = os.path.splitext(os.path.basename(graph_path))
+    bn = os.path.basename(graph_path)
+    graph_name, _ = os.path.splitext(bn)
+
+    writeGraph(best_result, os.path.join(args.output_dir, bn), Format.METIS)
 
     print("{},{}".format(graph_name, min_k))
